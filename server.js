c7af76ac93ccb1232d1fe1adbd3ef58ed194cdc8
Seiji Osada <seiji13r@outlook.com>
2018-11-10 12:57:19 -0600

    Cancel implemented

--- server.js
+++ server.js
@@ -7,8 +7,46 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var tables = [];
-var waitlist = [];
+var tables = [
+    { 
+    customerName: 'Seiji',
+    phoneNumber: '111-111-111',
+    customerEmail: 'seiji@seiji.com',
+    customerID: '123' 
+    },
+    { 
+        customerName: 'Alix',
+        phoneNumber: '111-111-111',
+        customerEmail: 'alix',
+        customerID: '124' 
+    },
+    {
+        customerName: 'Robert',
+        phoneNumber: '111-111-111',
+        customerEmail: 'seiji@seiji.com',
+        customerID: '125' 
+    },
+    {
+        customerName: 'Jona',
+        phoneNumber: '111-111-111',
+        customerEmail: 'XXXX',
+        customerID: '126' 
+    },
+    {
+        customerName: 'Snoopy',
+        phoneNumber: '111-111-111',
+        customerEmail: 'XXXX',
+        customerID: '127' 
+    },
+];
+var waitlist = [
+    {
+        customerName: 'Carlitos',
+        phoneNumber: '111-111-111',
+        customerEmail: 'XXXX',
+        customerID: '130' 
+    },
+];
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
@@ -25,6 +63,10 @@ app.get("/api/tables", function (req, res) {
 });
 app.get("/api/waitlist", function (req, res) {
     return res.json(waitlist);
+});
+
+app.post("/api/clear", function (req, res) {
+    tables = [];
 });
 
 app.post("/api/tables", function (req, res) {
@@ -46,6 +88,26 @@ app.post("/api/tables", function (req, res) {
     }
 
     // res.json(newTable);
+});
+
+app.post("/api/cancel", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var idObj = req.body;
+    id=idObj.id;
+
+    for (var i=0; i < tables.length ; i++){
+        if(tables[i].customerID===id){
+            tables.splice(i, 1);
+        }
+
+        if(waitlist[0]){
+            temp = waitlist.shift();
+            tables.push(temp);
+        }
+
+    }
+    res.json("Success!!");
 });
 
 app.listen(PORT, function () {
